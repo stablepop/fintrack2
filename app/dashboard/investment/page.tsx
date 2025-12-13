0a68e3fa3bdc5f795f8a268132cd21192dea817b
Ramiz1323 <reenabibi5678@gmail.com>
2025-12-13 12:19:29 +0530

    Some Edits

--- app/dashboard/investment/page.tsx
+++ app/dashboard/investment/page.tsx
@@ -3,13 +3,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
